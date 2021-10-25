cfcd438d10dbc3a2e5cc0810514cda9294e65739
[nicole parra] <[nicoleparrae@gmail.com]>
2021-10-25 18:20:07 -0400

    Create random number that represents winning box

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 // Create an Array of at least 3 losing messages
-
+var messages = [
+    'oops, try again',
+    'better luck next time',
+    'not quite right...'
+]
 // Create variables to count wins and losses
+var wins = 0
+var losses = 0
 
 // Create variables that target elements with the following IDs: 'message', 'wins', 'losses'
+var messageEl = document.getElementById('message')
+var winsEl = document.getElementById('wins')
+var lossesEl = document.getElementById('losses')
 
 // target all .box elements and attach a click event listener to each one using a loop
+var boxElements = document.querySelectorAll('.box')
+
+for (var i = 0; i < boxElements.length; i++){
+    //0,1,2
+    var boxElement = boxElements[i];
+    console.log(boxElements[i])
+    boxElement.onclick = function(event){
 
 // within each click event...
 // determine which box was clicked with 'this.textContent' or event.target.textContent
 // convert that value to a Number and store it to a variable
+    var numWasClicked = Number(event.target.textContent)
+    //console.log(typeof boxNumWasClicked)
+    console.log(Number(numWasClicked))
+    // create a random number between 1-3 and store it to a variable
+    var randomNumber = Math.floor(Math.random() * 4);
+    console.log(randomNumber)
+    // This number will represent the winning box
+        // determine if the box clicked is equal to the random number
+        if (numWasClicked === randomNumber) {
+            console.log('You WIN!!!!')
 
-// create a random number between 1-3 and store it to a variable
-// This number will represent the winning box
+            // if the numbers match, display a winning message by changing the text content of the div#message element
+            // if the numbers match, increment wins and display the win count in div#wins
+            
+        } else {
+            console.log('Try again.')
+            
+            // if the numbers don't match, change the div#message element's text to a random losing message from the array above
+            // if the numbers don't match, increment losses and display the loss count in div#losses
+        }
+    }
+}
 
-// determine if the box clicked is equal to the random number
-// if the numbers match, display a winning message by changing the text content of the div#message element
-// if the numbers match, increment wins and display the win count in div#wins
 
-// if the numbers don't match, change the div#message element's text to a random losing message from the array above
-// if the numbers don't match, increment losses and display the loss count in div#losses
+
+
